a54688ba8e7654114bcba99159dff827eed6a36d
oskarc35 <labourers_aslinux@aleeas.com>
2025-12-19 14:20:26 +0100

    fix gitignore

---
+++ WeatherForecast/Program.cs
@@ -0,0 +1,36 @@
+namespace WeatherForecast;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        //TODO: remove this after making sure the connection string is resolved correctly 
+        Console.WriteLine("--------------------------------------------------");
+        Console.WriteLine($"[DEBUG] Connection String: {connString}");
+        Console.WriteLine("--------------------------------------------------");
+        
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
