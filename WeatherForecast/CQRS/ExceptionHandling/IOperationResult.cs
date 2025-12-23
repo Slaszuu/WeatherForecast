1347b43a914485dc4e7524dfc02f4584429a1549
Slaszuu <bazyl1001@gmail.com>
2025-12-23 15:52:53 +0100

    ExceptionHandling directory hasbeen deleted

--- WeatherForecast/CQRS/ExceptionHandling/IOperationResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WeatherForecast.CQRS.ExceptionHandling;
-
-public interface IOperationResult
-{
-    OperationStatus Status { get; set; }
-    string? ExceptionMessage { get; set; }
-}
--- WeatherForecast/CQRS/ExceptionHandling/OperationResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WeatherForecast.CQRS.ExceptionHandling;
-
-public class OperationResult<T> : IOperationResult
-{
-    public OperationStatus Status { get; set; }
-    public string? ExceptionMessage { get; set; }
-}
